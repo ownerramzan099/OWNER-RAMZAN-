34cf15f68d0011e6d0b68c7d3568585a5c48f3b4
Shaan Personal bat <b8465391@gmail.com>
2025-02-26 18:30:00 +0500

    Update 0_girl_videos.js

--- Priyansh/commands/0_girl_videos.js
+++ Priyansh/commands/0_girl_videos.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮 :"];
+   var hi = ["☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𒁍 ⃝𝐎𝐖𝐍𝐄𝐑 ⸙𓆩𝐇𝐀𝐍𝐙𝐔𓆪 𓆣 »❯ :"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
