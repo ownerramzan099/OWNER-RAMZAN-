66ec13e4dab53225152351817a7dceab0bb63ccd
Shaan Personal bat <b8465391@gmail.com>
2025-04-10 18:05:34 +0500

    Create 0_#Hanzu.js

---
+++ Priyansh/commands/0_#Hanzu.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "HANZU",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("shan khan") ||
+     react.includes("Shaan Khan") || react.includes("HANZU") || react.includes("hanzala") ||
+react.includes("HANZALA-KING") ||
+react.includes("HAMZA") ||     
+react.includes("hanzala King")) {
+    var msg = {
+        body: `${name} 𝑻𝑼𝑴 𝑵𝑬 𝑩𝑯𝑼𝑳𝑨𝒀𝑨 𝑶𝑼𝑹 𝑯𝑼𝑴 𝑪𝑯𝑨𝑳𝑬 𝑨𝑨𝒀𝑬💞✿`,attachment: fs.createReadStream(__dirname + `/noprefix//`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }
